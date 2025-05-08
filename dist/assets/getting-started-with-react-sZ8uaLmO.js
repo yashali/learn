fc8f1df037a3bc9e5c69f38d330f41d58f1af625
Yashali Bansod <yashali.bansod@msci.com>
2025-05-08 09:50:06 +0530

    relative path for images

--- dist/assets/getting-started-with-react-sZ8uaLmO.js
+++ dist/assets/getting-started-with-react-sZ8uaLmO.js
@@ -4,12 +4,12 @@ excerpt: "Learn the basics of React and how to create your first application"\r
 date: "2024-03-20"\r
 tags: ["react", "javascript", "web-development"]\r
 author: "Your Name"\r
-coverImage: "/images/react-cover.jpg"\r
+coverImage: "images/react-cover.jpg"\r
 ---\r
 \r
 # Getting Started with React\r
 \r
-![React Logo](/images/react-logo.png)\r
+![React Logo](images/react-logo.png)\r
 \r
 React is a powerful JavaScript library for building user interfaces. In this post, we'll explore the basics of React and how to get started with your first application.\r
 \r
@@ -17,7 +17,7 @@ React is a powerful JavaScript library for building user interfaces. In this pos
 \r
 React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you create reusable UI components.\r
 \r
-![React Components](/images/react-components.gif)\r
+![React Components](images/react-components.gif)\r
 \r
 ## Why React?\r
 \r
@@ -63,4 +63,4 @@ function Welcome() {\r
 \r
 React is an excellent choice for building modern web applications. Its component-based architecture and virtual DOM make it efficient and maintainable.\r
 \r
-![React Success](/images/react-success.gif) `;export{e as default};
+![React Success](images/react-success.gif) `;export{e as default};
